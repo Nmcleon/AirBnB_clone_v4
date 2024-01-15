1a9efeb83b1a912e4e17b659f4ff44bd6ed4d9f7
N <51336368+Nmcleon@users.noreply.github.com>
2024-01-15 12:09:25 +0200

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,6 +1,6 @@
-/* Dynamic Funcionality */
+/* API status */
 $('document').ready(function () {
-  /* Listens for changes on each INPUT checkbox tag */
+  /* JavaScript script (static/scripts/1-hbnb.js): */
   const amenitiesId = {};
   $('INPUT[type="checkbox"]').click(function () {
     if ($(this).prop('checked')) {
@@ -10,16 +10,10 @@ $('document').ready(function () {
     }
     $('.amenities h4').text(Object.values(amenitiesId).join(', '));
   });
-  /* Get status of api and change class if api not available
-  Request status every 10 seconds */
-  /* Simple way, manual re-loading page: */
-  /* $.get(`http://${window.location.hostname}:5001/api/v1/status/`, function(status){
-    if (status.status === 'OK') {
-      $('DIV#api_status').addClass('available');
-    } else {
-       $('DIV#api_status').removeClass('available');
-    }
-  });
+  /* Update the API entry point (api/v1/app.py) by replacing the current
+  CORS CORS(app, origins="0.0.0.0") by CORS(app, resources={r"/api/v1/*": {"origins": "*"}}).
+Change the route 1-hbnb to 2-hbnb in the file 2-hbnb.py (based on 1-hbnb.py)
+Create a new template 2-hbnb.html (based on 1-hbnb.html) and update it
   */
   const callout = function () {
     $.ajax({
@@ -43,7 +37,7 @@ $('document').ready(function () {
   };
   callout();
   /*
-    Retrieve all places and create a articule tag with them
+    Retrieve all places and create a articule tag
   */
   const getPlaces = function () {
     $.ajax({
@@ -56,32 +50,32 @@ $('document').ready(function () {
         $.each(places, function (index, place) {
           $('.places').append(
             '<article>' +
-            '<div class="title_box">' +
-            '<h2>' + place.name + '</h2>' +
-            '<div class="price_by_night">' + '$' + place.price_by_night +
-            '</div>' +
-            '</div>' +
-            '<div class="information">' +
-            '<div class="max_guest">' +
-            '<br />' + place.max_guest + ' Guests' +
-            '</div>' +
-            '<div class="number_rooms">' +
-            '<br />' + place.number_rooms + ' Bedrooms' +
-            '</div>' +
-            '<div class="number_bathrooms">' +
-            '<br />' + place.number_bathrooms + ' Bathroom' +
-            '</div>' +
-            '</div>' +
-            '<div class="description">' + place.description +
-            '</div>' +
-            '</article>');
+              '<div class="title_box">' +
+              '<h2>' + place.name + '</h2>' +
+              '<div class="price_by_night">' + place.price_by_night +
+              '</div>' +
+              '</div>' +
+              '<div class="information">' +
+              '<div class="max_guest">' +
+              '<br />' + place.max_guest + ' Guests' +
+              '</div>' +
+              '<div class="number_rooms">' +
+              '<br />' + place.number_rooms + ' Bedrooms' +
+              '</div>' +
+              '<div class="number_bathrooms">' +
+              '<br />' + place.number_bathrooms + ' Bathroom' +
+              '</div>' +
+              '</div>' +
+              '<div class="description">' + place.description +
+              '</div>' +
+              '</article>');
         });
       }
     });
   };
   getPlaces();
   /*
-    Filter places by amenities on button search click
+    Filter places by amenities
   */
   $('button').click(function () {
     $.ajax({
