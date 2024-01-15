55b63dec4be14a8656772e62a595b732cd57e05e
N <51336368+Nmcleon@users.noreply.github.com>
2024-01-15 09:26:49 +0000

    add cache id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -39,9 +39,10 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
+                           cache_id=uuid.uuid4()
                            places=places)
 
 
     if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+        """ Main Function """
+        app.run(host='0.0.0.0', port=5000)
